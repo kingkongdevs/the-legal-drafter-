4631a798a0ec94844ae16670fc32914695ad84e4
Luke Crawford <luke.c@kingkong.com.au>
2020-03-19 14:52:31 +1100

    stop accordion from bouncing when clicking an open item. Also remove active class when another item is activated

--- assets/src/js/home.js
+++ assets/src/js/home.js
@@ -4,8 +4,16 @@
     $('.accordion dt.active').next().slideDown()
     
     $('.accordion').on('click', 'dt', function() {
-        $('.accordion dd').slideUp();  
-        $(this).toggleClass('active').next().slideDown();
+        $('.accordion dd').slideUp(); 
+
+        if(!$(this).hasClass('active')){
+            // remove active class from any others that might be active
+            $('.accordion dt.active').removeClass('active');
+            $(this).addClass('active');
+            $(this).next().slideDown();
+        }else{
+            $(this).removeClass('active');
+        }
     });  
 
     // Slick Slider (jQuery) - Remove these if not in use 
