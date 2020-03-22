0d1499dba870e6b4075b008f446aae681bf23c5e
Luke Crawford <luke.c@kingkong.com.au>
2020-03-22 12:15:31 +1100

    video overlay z-index

--- assets/src/js/home.js
+++ assets/src/js/home.js
@@ -185,6 +185,7 @@
                     var iframeHTML = '<iframe src="https://www.youtube.com/embed/' + $(this).attr('data-video-id') + '?title=0&byline=0&portrait=0?&autoplay=1" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>';
 
                     $(this).parent('.video-preview-container').find('.inline-video-trigger').hide();
+                    $(this).parent('.video-preview-container').find('.overlay').hide();
                     $(this).parent('.video-preview-container').find('iframe').remove();
                     $(this).parent('.video-preview-container').append(iframeHTML);
                 } else {
