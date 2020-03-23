0c55b47f8b33c66e348671708ae9ec746b9793b5
Nikita <nikita.s@kingkong.com.au>
2020-03-24 09:25:21 +1100

    Updated checklist module to have list styles

--- assets/src/js/home.js
+++ assets/src/js/home.js
@@ -290,3 +290,14 @@
     })
 
 })( jQuery );
+
+// Get Current Month Name
+function getCurrentMonth() {
+    var months    = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+    var now       = new Date();
+    var thisMonth = months[now.getMonth()]; // getMonth method returns the month of the date (0-January :: 11-December)
+    var output = document.getElementsByClassName('output');
+
+    $( ".output" ).html( thisMonth);
+}
+getCurrentMonth();
