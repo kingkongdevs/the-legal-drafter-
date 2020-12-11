d6e36e553100e8b8b0a9e5e6a1e32a28b30b1164
Nikita <nikita.s@kingkong.com.au>
2020-12-11 15:11:52 +1100

    Added flipclock timer, updated javascript & removed minor issues, updates all the styles to remove duplicates & extras, updated all the icons to be svgs/pngs instead of font-awesome, removed error from multistep form, updated all the pages according to common CRO requests

--- assets/src/js/home.js
+++ assets/src/js/home.js
@@ -1,5 +1,4 @@
 (function($) {
-
     $(window).on('load', function () {
 
         // Components loading animations
@@ -24,24 +23,6 @@
             }
         });
 
-        // Inline Video Funcionality
-        $(document).on('click', '.inline-video-trigger', function () {
-            if ($(this).data('video-id')) {
-                if ($(this).hasClass('vimeo')) {
-                    var iframeHTML = '<iframe src="https://www.vimeo.com/embed/' + $(this).attr('data-video-id') + '?title=0&byline=0&portrait=0?&autoplay=1" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>';
-                } else {
-                    var iframeHTML = '<iframe src="https://www.youtube.com/embed/' + $(this).attr('data-video-id') + '?autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
-                }
-
-                $(this).parent('.video-preview-container').find('.inline-video-trigger').hide();
-                $(this).parent('.video-preview-container').find('.overlay').hide();
-                $(this).parent('.video-preview-container').find('iframe').remove();
-                $(this).parent('.video-preview-container').append(iframeHTML);
-            } else {
-                console.error('no video ID provided.');
-            }
-        });
-
         // Phone Concatenation Script For Tracking
         setTimeout(function () {
             $('.phone-text em').each(function () {
@@ -58,61 +39,7 @@
                 });
             });
 
-        }, 3000)
-
-        //COUNTDOWN
-        if ($('.countdown-timer').length > 0) {
-            var date = new Date();
-            var startMinutes = date.getMinutes();
-            var startSeconds = date.getSeconds();
-            var remainingSeconds = 59 - startSeconds;
-            var remainingMinutes = 0;
-
-            if (startMinutes < 15) { remainingMinutes = 15 - startMinutes - 1; }
-            else if (startMinutes < 30) { remainingMinutes = 30 - startMinutes - 1; }
-            else if (startMinutes < 45) { remainingMinutes = (45 - startMinutes - 1); }
-            else { remainingMinutes = (60 - startMinutes - 1); }
-
-
-            var count = (remainingMinutes * 60 + remainingSeconds);
-            var counter = setInterval(timer, 1000); //1000 will run it every 1 second
-            
-            function progress(timeleft, timetotal) {
-                var progressBarWidth = 100 - ((timeleft / timetotal) * 100);
-                $('.progressbar .completed').animate({ width: progressBarWidth + '%' }, 500);
-            }
-            function timer() {
-                count = count - 1;
-                if (count == -1) {
-                    clearInterval(counter);
-                    return;
-                }
-
-                var seconds = count % 60;
-                var minutes = Math.floor(count / 60);
-                var hours = Math.floor(minutes / 60);
-                minutes %= 60;
-                hours %= 60;
-
-                var minuteString = ' minutes and ';
-                if (minutes == 1) { minuteString = ' minute and ' }
-                var secondString = ' seconds';
-                if (seconds == 1) { secondString = ' second' }
-
-                progress((minutes * 60 + seconds), 900);
-                document.querySelectorAll(".training-text .time-left").forEach(function (selected, index) {
-                    if (seconds < 1 && minutes < 1) {
-                        selected.innerHTML = $(selected).closest('.countdown-timer').data('end-text');
-                    }
-                    if (minutes < 1) {
-                        selected.innerHTML = seconds + secondString; // watch for spelling
-                    } else {
-                        selected.innerHTML = minutes + minuteString + seconds + secondString; // watch for spelling
-                    }
-                });
-                
-            }
-        }
+        }, 2000)
 
         // Slick Slider (jQuery) - Remove these if not in use 
         $('.testimonials .slider').slick({
@@ -120,7 +47,7 @@
             slidesToShow: 1,
             slidesToScroll: 1,
             speed: 600,
-            dots: true,
+            dots: false,
             autoplay: true,
             autoplaySpeed: 5000,
             prevArrow: $('.testimonials .prev'),
@@ -152,7 +79,6 @@
         $('.video-gallery .slider').slick({
             slidesToShow: 1,
             slidesToScroll: 1,
-            lazyLoad: 'progressive',
             speed: 600,
             prevArrow: $('.video-gallery .prev'),
             nextArrow: $('.video-gallery .next'),
@@ -164,11 +90,11 @@
             slidesToShow: 4,
             slidesToScroll: 1,
             asNavFor: '.video-gallery .slider',
-            lazyLoad: 'progressive',
-            dots: false,
             arrows: false,
+            dots: false,
+            centerMode: true,
             focusOnSelect: true,
-            infinit: true,
+            infinit: true
         });
     })
 
@@ -220,12 +146,6 @@
                     {
                         breakpoint: 768,
                         settings: 'unslick'
-                    },
-                    {
-                        breakpoint: 576,
-                        settings: {
-                            slidesToShow: 1,
-                        }
                     }
                 ]
             });
@@ -250,6 +170,24 @@
             $('html, body').animate({
                 scrollTop: $(target).offset().top - 80
             }, 1500)
+        }
+    });
+
+    // Inline Video Funcionality
+    $(document).on('click', '.inline-video-trigger', function () {
+        if ($(this).data('video-id')) {
+            if ($(this).hasClass('vimeo')) {
+                var iframeHTML = '<iframe src="https://player.vimeo.com/video/' + $(this).attr('data-video-id') + '?title=0&byline=0&portrait=0?&autoplay=1" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>';
+            } else {
+                var iframeHTML = '<iframe src="https://www.youtube.com/embed/' + $(this).attr('data-video-id') + '?autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+            }
+
+            $(this).parent('.video-preview-container').find('.inline-video-trigger').hide();
+            $(this).parent('.video-preview-container').find('.overlay').hide();
+            $(this).parent('.video-preview-container').find('iframe').remove();
+            $(this).parent('.video-preview-container').append(iframeHTML);
+        } else {
+            console.error('no video ID provided.');
         }
     });
 
--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -2,18 +2,18 @@
 import './vendor/jquery.min.js';
 import './vendor/jquery.viewportchecker.min.js';
 // import './vendor/bootstrap.min.js';
-// import './vendor/jquery.lazy.min.js';
-import './vendor/lazysizes.min.js';
 // import './vendor/jquery.validate.min.js';
 import './vendor/slick.min.js';
 
+import './vendor/lazysizes.min.js';
 import './images.js';
+
 /** CUSTOM SCRIPTS **/
 import './home.js';
+// import './timer.js';
 
 /** EBOOK PAGE SPECIFIC SCRIPTS **/
 // import './ebook.js';
-
 
 /** QUSETIONNAIRE PAGE SPECIFIC SCRIPTS **/
 // import './vendor/jquery.easing.min.js';
