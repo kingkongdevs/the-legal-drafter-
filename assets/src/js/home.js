57cb22f64fdb6352f24bae514d9144a663ba6667
Jake <jake.n@kingkong.com.au>
2022-01-31 09:56:21 +1100

    Form Redirect

--- assets/src/js/home.js
+++ assets/src/js/home.js
@@ -1,7 +1,8 @@
 window.contentLoaded(window, function (e) {
     //Add your project specific scripts here
-    
+
 });
+
 
 // Get Current Month Name
 function getCurrentMonth() {
@@ -9,7 +10,51 @@ function getCurrentMonth() {
     var now       = new Date();
     var thisMonth = months[now.getMonth()]; // getMonth method returns the month of the date (0-January :: 11-December)
     var output = document.getElementsByClassName('output');
-
+    
     $( ".output" ).html( thisMonth);
 }
 getCurrentMonth();
+
+
+    initForm()
+    var form = document.getElementById("offer-form");
+    console.log(form);
+
+function initForm () {
+    
+    console.log('Form Called');
+    if (!form) return
+
+    $(form).validate({
+      submitHandler: async function (form, event) {
+        event.preventDefault()
+
+        var btn = $(form).find('[type="submit"]')
+        btn.disabled = true
+        btn.html('Sending...')
+
+        var data = new FormData(event.target)
+        fetch(event.target.action, {
+          method: form.method,
+          body: data,
+          headers: {
+            Accept: 'application/json'
+          }
+        })
+          .then(response => {
+            /* if (typeof window.dataLayer != 'undefined') {
+              window.dataLayer.push({
+                event: 'gtm.formSubmission',
+                form: form
+              })
+            } */
+            window.location.href = window.location.href + '/thank-you'
+          })
+          .catch(error => {
+            form.html(
+              '<p class="error">There was an error sending your message. Please try again later.</p>'
+            )
+          })
+      }
+    })
+  }
